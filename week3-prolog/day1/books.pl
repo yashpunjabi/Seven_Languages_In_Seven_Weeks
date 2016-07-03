a8281610f89a7619b3e1cf24134090ab27b4b373
Yash Punjabi <yashrpunjabi@gmail.com>
2016-07-03 16:16:46 -0500

    day1 prolog

---
+++ week3-prolog/day1/books.pl
@@ -0,0 +1,5 @@
+author(a_game_of_thrones, george_r_r_martin).
+author(the_winds_of_winter, george_r_r_martin).
+author(a_feast_for_crows, george_r_r_martin).
+author(harry_potter, j_k_rowling).
+author(clean_code, robert_martin).
---
+++ week3-prolog/day1/musicians.pl
@@ -0,0 +1,11 @@
+uses(kirk, guitar).
+uses(i_dunno, guitar).
+uses(santana, guitar).
+uses(lars, drums).
+uses(taylor, vocals).
+
+genre(kirk, metal).
+genre(santana, rock).
+genre(i_dunno, some_genre).
+genre(lars, metal).
+genre(taylor, pop).
