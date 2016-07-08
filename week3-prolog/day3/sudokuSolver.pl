e31ef00c8efde11860db6a9627b2eb0912612c9e
Yash Punjabi <yashrpunjabi@gmail.com>
2016-07-08 00:33:28 -0500

    Work on solution instead of working on unsolved

--- week3-prolog/day3/sudokuSolver.pl
+++ week3-prolog/day3/sudokuSolver.pl
@@ -1,5 +1,13 @@
-valid(Sudoku):-
-    Sudoku = [S11, S12, S13, S14, S15, S16, S17, S18, S19,
+all_different([]).
+all_different([Head|Tail]) :-
+    fd_all_different(Head),
+    all_different(Tail).
+
+
+sudokuSolver(Unsolved,Solution):-
+    Solution = Unsolved,
+
+    Solution = [S11, S12, S13, S14, S15, S16, S17, S18, S19,
               S21, S22, S23, S24, S25, S26, S27, S28, S29,
               S31, S32, S33, S34, S35, S36, S37, S38, S39,
               S41, S42, S43, S44, S45, S46, S47, S48, S49,
@@ -8,18 +16,8 @@ valid(Sudoku):-
               S71, S72, S73, S74, S75, S76, S77, S78, S79,
               S81, S82, S83, S84, S85, S86, S87, S88, S89,
               S91, S92, S93, S94, S95, S96, S97, S98, S99],
-    fd_domain(Sudoku, 1, 9).
-
-
-all_different([]).
-all_different([Head|Tail]) :-
-    fd_all_different(Head),
-    all_different(Tail).
-
-
-sudokuSolver(Unsolved,Solution):-
-    valid(Unsolved),
-    Solution = Unsolved,
+              
+    fd_domain(Solution, 1, 9),
 
     Row1 = [S11, S12, S13, S14, S15, S16, S17, S18, S19],
     Row2 = [S21, S22, S23, S24, S25, S26, S27, S28, S29],
