69d49c5e9afb18e3c96315745c2b968f5a87ee24
Yash Punjabi <yashrpunjabi@gmail.com>
2016-07-27 00:53:26 -0500

    Clojure day2

--- week6-clojure/day2/src/day2/core.clj
+++ week6-clojure/day2/src/day2/core.clj
@@ -1,6 +1,24 @@
 (ns day2.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Vehicle
+    (speed [this])
+    (accelerate [this])
+    (decelerate [this]))
+
+(defrecord Car [speed]
+    Vehicle
+    (speed [_] speed)
+    (accelerate [_] (Car. (+ speed 10)))
+    (decelerate [_] (Car. (- speed 10))))
+
+(defmacro unless [test body_false body_true]
+    (list 'if (list 'not test) body_false body_true))
+
+(defn -main
+  []
+  (println (unless true "not this" "this"))
+  (println (unless false "this" "not this"))
+  (def car (Car. 50))
+  (println (speed car))
+  (println (speed (accelerate car)))
+  (println (speed (decelerate car))))
